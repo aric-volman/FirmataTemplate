ceb3cd1ef6c105411c935dc844dc52129460384e
aric-volman <89823522+aric-volman@users.noreply.github.com>
2023-04-06 18:53:05 -0700

    Blink example
    Creates Blink code that blinks every half second. Uses Pin 13 to blink Arduino LED.

--- main/src/main/java/com/option16/App.java
+++ main/src/main/java/com/option16/App.java
@@ -2,28 +2,45 @@ package com.option16;
 
 // Imports Firmata4J
 import org.firmata4j.firmata.*;
+
+import java.io.IOException;
+
 import org.firmata4j.IODevice;
+import org.firmata4j.Pin.Mode;
 
 /**
  * Hello world!
- *
+ * Connects to Firmata Firmware and runs Blink via Serial connection
  */
 public class App 
 {
+    public static final int blinkPin = 13; // In-built LED
     public static void main( String[] args )
     {
         IODevice arduino = new FirmataDevice("COM3");
         System.out.println( "Hello World!" );
 
         try {
-            arduino.start();       // start comms with board;
+            arduino.start();
             System.out.println("Board started.");
             arduino.ensureInitializationIsDone();
+        } catch (IOException | InterruptedException e) {
+            System.out.println("ERROR: Arduino start failed.");
+            e.printStackTrace();
         }
-        catch (Exception ex) {
-            System.out.println("couldn't connect to board.");
-        } finally {
-            System.out.println("I love Firmata!");
+
+        try {
+            // PUT ARDUINO CODE HERE
+            arduino.getPin(blinkPin).setMode(Mode.OUTPUT);
+            while (true) {
+                // Blnks every half-second
+                arduino.getPin(blinkPin).setValue(1);
+                Thread.sleep(500);
+                arduino.getPin(blinkPin).setValue(0);
+                Thread.sleep(500);
+            }
+        } catch (IllegalStateException | IOException | InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
